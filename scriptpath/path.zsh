e9b85a2ae0ed0fe8760a7bce52d733375de8c49d
John Winsor <johnwinsor@gmail.com>
2015-07-13 13:25:00 -0700

    added a scriptpath

---
+++ scriptpath/path.zsh
@@ -0,0 +1,2 @@
+export SCRIPTPATH="$HOME/Dropbox/Mills/scripts:$HOME/Dropbox/Mills/scripts/marcscripts:$HOME/Dropbox/Mills/scripts/marcupdates:$HOME/Dropbox/Mills/scripts/oclc:$HOME/yaz/bin"
+export PATH="$SCRIPTPATH:$PATH"
