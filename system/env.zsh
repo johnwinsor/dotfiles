f9ed5e254c6b185f9f427aae7dec043e2a978a8c
John Winsor <johnwinsor@gmail.com>
2015-07-13 13:02:13 -0700

    added paths to scripts in system/env.zsh. Added new vimrc

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,2 @@
 export EDITOR='atom'
+export PATH="$HOME/Dropbox/Mills/scripts:$HOME/Dropbox/Mills/scripts/marcscripts:$HOME/Dropbox/Mills/scripts/marcupdates:$HOME/Dropbox/Mills/scripts/oclc:$HOME/yaz/bin:$PATH"
